682657154256b0ebfca7ba1d9007b14040baf534
i-Rony-op <raunaksharma6205@gmail.com>
2024-01-25 20:44:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true,
+// });
+
+function pg4Animation() {
+  var elem = document.querySelector("#elem-cont");
+  var fix = document.querySelector("#fixed-image");
+
+  elem.addEventListener("mouseenter", function () {
+    fix.style.display = "block";
+  });
+
+  elem.addEventListener("mouseleave", function () {
+    fix.style.display = "none";
+  });
+
+  var elems = document.querySelectorAll(".elem");
+  elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      var image = e.getAttribute("data-image");
+      fix.style.backgroundImage = `url(${image})`;
+    });
+  });
+}
+pg4Animation();
+
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 40,
+  });
+}
+swiperAnimation();
+function loader() {
+  var loader = document.querySelector("#loader");
+
+  setTimeout(function () {
+    loader.style.top = "-100%";
+  }, 4000);
+}
+
+loader();
